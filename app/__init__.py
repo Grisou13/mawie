e62827a0569ef088e0aef82c8ebe0a09ac3b046e
Eric <paralysant@gmail.com>
2016-09-02 12:21:28 +0200

    added explorer main structure

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,12 @@
-import .main
+import app.main
+from app.explorer.explorer import *
 
 def run():
     app = main.App()
-    app.boot()
+
+    #only for test the explorer class with original path (mawie file)
+
+
+    # coding=utf-8
+    explorer = Explorer("stubs/FILM_a_trier")
+    print(explorer.nameParsing(explorer.getFolderContent()))
