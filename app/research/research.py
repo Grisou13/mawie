8a0e5a326d47b5a18a77cede4d32653817fda5ac
Grisou13 <grisou13@hotmail.fr>
2016-09-16 12:08:12 +0200

    filtering with db columns works + added singleton to helpers
    added the compoenent. Now gonna work on it

--- app/research/research.py
+++ app/research/research.py
@@ -1,4 +1,7 @@
+from sqlalchemy import Column
+
 from app.models.Movie import Movie
+from app.models import db
 class Searchable(object):
     """ Prototype for any searchable item """
 
@@ -25,16 +28,30 @@ class Research:
 
         m = self.model
         if filters is None:
-            return m.query().filter(m.name.like("%"+query+"%"),m.desc.like("%"+query+"%"))
+            for mov in m.query().filter(m.name.like("%"+query+"%"),m.desc.like("%"+query+"%")):
+                yield mov
         else:
-            if filter in m.columns():
-                return m.query.filter(m.columns())
+            if isinstance(filters,list):
+                for f in filters:
+                    yield self.filter(f,query)
             else:
-                raise RuntimeError("Well the filter doesnt exist you know?")
+                yield self.filter(filters,query)
         # query local db
         # query remote db
         pass
-
+    def getFields(self,cls):
+        _ = {}
+        for col in cls.__table__.columns:
+            name = str(col).split(".")[1]
+            _[name.lower()] = name
+        return _
+    def filter(self,f,query):
+        _f = self.getFields(self.model)
+        print(_f)
+        if f.lower() in _f.keys():
+            return list(self.model.query().filter(getattr(self.model, str(_f[f.lower()])).like("%" + query + "%")))
+        else:
+            raise RuntimeError("Well the filter doesnt exist you know?")
     async def localSearch(self):
         pass
     async def remoteSearch(self):
@@ -42,4 +59,4 @@ class Research:
 
 if __name__ == '__main__':
     r = Research()
-    r.search("The")
+    print(r.search("Sayo Saruta","Actors").__next__())
