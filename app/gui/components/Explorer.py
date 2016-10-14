0fd4746a858033f09b99d654aa632abe112db324
Grisou13 <grisou13@hotmail.fr>
2016-10-14 15:02:07 +0200

    initial commit

--- app/gui/components/Explorer.py
+++ app/gui/components/Explorer.py
@@ -2,4 +2,18 @@ import tkinter
 import tkinter.ttk
 from app.gui.components import GuiComponent
 
-class ExplorerFrame():pass
+
+class ExplorerFrame(tkinter.Frame,GuiComponent):
+    def __init__(self, gui, *arg, **kwargs):
+        self.gui = gui
+        self.gui.register_listener(self)
+        super(ExplorerFrame, self).__init__(gui.root, *arg, **kwargs)
+        self.grid()
+        self.list = tkinter.Listbox()
+        self.list.pack()
+
+    def requestAction(self, actionName):
+        pass
+
+    def handleAction(self, actionName, data):
+        pass
