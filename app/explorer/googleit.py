6293227d90c209bf037a2017db9afb84b7759375
ericBroutba <eric.bousbaa@hotmail.com>
2016-11-17 22:35:36 +0100

    Google it WIP With Scrapy
    Still some issues here.

--- app/explorer/googleit.py
+++ app/explorer/googleit.py
@@ -1,8 +1,6 @@
-import json
-import urllib.request
-import sys
 
-"""
+
+'''
     PLEASE READ HERE FIRST
 
     the duckduckgo api that I'm using doesn't handle the "non obvious queries"
@@ -13,11 +11,84 @@ import sys
     and have a live example (that doesn't work (that's the point...))
     https://duckduckgo.com/?q=very%20bad%20trip%3Aimdb&format=json
 
-    HF ! 
-""""
+    HF !
+'''
 
 
+import sys
+import requests
+import os
+import urllib
+from bs4 import BeautifulSoup
+#do a pip install PyExecJS
+#import execjs
 
+
+# -*- coding: utf-8 -*-
+import scrapy
+
+
+class SpiderDuck(scrapy.Spider):
+    name = "SpiderDuck"
+
+    def makeRequest(self, url):
+        #assert type(url) is str
+        return scrapy.Request(url, self.parse)
+
+    def parse(self, response):
+
+        for link in response.xpath("result__a").extract():
+            item = dict()
+            item['url'] = link.xpath("@href"),extract_first()
+            yield item
+
+
+movieName = "Harry Potter"
+movieName.replace(" ", "%20")
+url = "http://duckduckgo.com/?q="+movieName+" %3Aimdb"
+
+
+sp = SpiderDuck()
+links = sp.makeRequest(url)
+
+print(links)
+
+"""
+def spider(url):
+    page = urllib.request(url)
+    #source_code = requests.get(url)
+    #plain_text = source_code.text
+    convert_data = BeautifulSoup(page)
+
+    print(convert_data)
+"""
+
+"""
+from bs4 import BeautifulSoup
+import requests
+import sys
+class googleIt:
+    def getMovie(self, movieName):
+        movieName.replace(" ", "%20")
+        url = "http://duckduckgo.com/?q="+movieName+" %3Aimdb"
+
+        src = requests.get(url)
+        plain = src.text
+
+        soup = BeautifulSoup(plainSrc)
+
+        print(soup)
+
+        #for link in soup.findAll("a", {'class': 'result__a'}):
+        for link in soup.findAll("a"):
+            print("hey !")
+            print(link)
+            #print(title)
+
+
+crawler = googleIt()
+crawler.getMovie("Harry potter")
+"""
 #http://api.duckduckgo.com/?q=x&format=json
 
 # mv = "very bad trip"
@@ -31,6 +102,7 @@ import sys
 # json.loads(data.decode(encoding))
 #
 
+'''
 class googleIt():
 
 
@@ -50,6 +122,4 @@ class googleIt():
 
     def checkWebsiteURl(self, url, website="imdb"):
         pass
-
-cls = googleIt()
-cls.getAMove("very bad trip")
+'''
