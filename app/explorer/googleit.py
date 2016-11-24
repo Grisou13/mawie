069dced4a87fa12ccda21d6f011b1c60cb2f595d
ericBroutba <eric.bousbaa@hotmail.com>
2016-11-24 22:41:14 +0100

    GoogleIT almost done
    Need more verifs with the imdb api

--- app/explorer/googleit.py
+++ app/explorer/googleit.py
@@ -1,17 +1,23 @@
 # pip install py_bing_search
+import re
+
 from py_bing_search import PyBingWebSearch
+#from py_bing_search import PyBingWebSearch
+from imdbpie import Imdb
+import Levenshtein
 import re
 import sys
-from imdbpie import Imdb
 
 
 class googleIt():
 
-    BING_API_KEY = "your api key here"
+    BING_API_KEY = "SjCn0rSMC6ipl8HJiI2vAYQj1REMPA+raOMPSd5K9A0"
     domainSearch = ""
-    imd
+    imdb = object()
+
     def __init__(self, domainSearch = "imdb"):
         self.domainSearch = domainSearch
+        self.imdb = Imdb()
 
     def _makeSearchTerm(self, movieName):
         return movieName +" :" +self.domainSearch
@@ -41,7 +47,6 @@ class googleIt():
 
         # TODO make pattern to find the imdb main url (ex: http://www.imdb.com/title/tt0330373/)
         # check http://daringfireball.net/2010/07/improved_regex_for_matching_urls
-        # pattern = re.compile(r'(?i)\b((?:[a-z][\w-]+:(?:/{1,3}|[a-z0-9%])|www\d{0,3}[.]|[a-z0-9.\-]+[.][a-z]{2,4}/)(?:[^\s()<>]+|\(([^\s()<>]+|(\([^\s()<>]+\)))*\))+(?:\(([^\s()<>]+|(\([^\s()<>]+\)))*\)|[^\s`!()\[\]{};:'".,<>?«»“”‘’]))')
 
         # if you give the format as http://www.imdb.com/title/tt0330373/, return the id
         # mess up if incorrect url. This is why we need a regex here
@@ -54,23 +59,90 @@ class googleIt():
 
         return movieId
 
+    def getMovieImage(self, movieId = "", movieTitle = ""):
+        pass
+
 
     def getMovieInfo(self, movieId = "", movieTitle = ""):
+        """
+            Return the IMDB's info of a movie
+
+            movieId (string) is the IMDB id (can be found in a IMDB url)
+            movieTitle (sting) is simple the movie
+            Only one parameter is required.
+            If both are given, we'll  check if movieId correspond to the movieTitle
+        """
+        def _hasNumber(inputStr):
+            """
+                Check if given sting contains any number (1-9).
+                Return boolean
+                Do you really need more details ?
+            """
+            return any(char.isdigit() for char in inputStr)
 
         assert(movieId or movieTitle)
+        assert isinstance(movieTitle, str)
+        movieTitle = movieTitle.lower()
 
-        #assert (not movieId) or (not movieTitle)
+        if movieTitle:
+
+            foundMovieInfo = self.imdb.search_for_title(movieTitle)
+            print(foundMovieInfo)
+            print(type(foundMovieInfo))
+            sys.exit("")
+
+            if isinstance(foundMovieInfo, list):
+                pass
+                
+            #print(foundMovieInfo)
+            for l in foundMovieInfo:
+
+                print(l)
+
+                #print(type(self.getMovieInfo(l.get("imdb_id"))))
+                #print(l.get("imdb_id"))
+                #print(self.getMovieInfo(l.get))
+                #print(self.imdb.get_title_by_id(l.get("imdb_id")))
+
+        elif movieId:
+            foundMovieTitle = self.imdb.get_title_by_id(movieId)
+            foundMovieInfo = self.imdb.search_for_title()
+
+            print()
+
+            foundMovieTitle = foundMovieInfo.title.lower()
+            """
+            # check if found movie correspond to the given one (if one given)
+            if movieTitle:
+                # if the given movie name contains a #, we won't do a Levenshtein
+                # Harry Potter 4 === Harry Potter and the Goblet of Fire
+                if not _hasNumber(movieTitle):
+                    # If no # in it, we'd do a little Levenshtein distance mesure
+                    # both movies name are in lowercase
+                    if Levenshtein.distance(foundMovieTitle, movieTitle) > 3:
+                        raise ValueError("Given movie title doesn't match the id.")
+            """
+            return foundMovieInfo
+
+        # if no given id, then try to find with title
+
+        else:
+            #wtf
+            return False
 
 
 
-googleItPutain = googleIt()
-#myId = google.getMovieID("Harry potter 4")
+if __name__ == "__main__":
+    googleItPutain = googleIt()
+    #myId = google.getMovieID("Harry potter 4")
 
-myFalseId = "tt0330373"
-googleItPutain.getMovieInfo(movieId = myFalseId)
-googleItPutain.getMovieInfo(movieTitle = "Harry potter")
-googleItPutain.getMovieInfo(movieId = myFalseId, movieTitle = "Harry potter")
-googleItPutain.getMovieInfo()
+    myFalseId = "tt0330373"
+    DonnotSpeakAboutIt = "tt0137523"
+
+    #googleItPutain.getMovieInfo(movieId = myFalseId)
+    googleItPutain.getMovieInfo(movieTitle = "Harry potter")
+    #googleItPutain.getMovieInfo(movieId = DonnotSpeakAboutIt, movieTitle = "Fight CLUB")
+    googleItPutain.getMovieInfo()
 
 """
 # pip install imdbpie
@@ -82,5 +154,4 @@ imdb = Imdb(anonymize=True) # to proxy requests
 res = imdb.search_for_title("Very bad trip 2")
 print("Pr out")
 print(res)
-
 """
