6adf9022e3a4299eed55dea2d5ccc8a4f58fae86
thomas ricci <grisou13@hotmail.fr>
2016-12-10 00:17:41 +0100

    renamed app to mawie

--- mawie/__init__.py
+++ mawie/__init__.py
@@ -3,7 +3,7 @@ import os
 
 sys.path.append(os.path.join(__file__, "../../"))
 sys.path.append(os.path.join(__file__, "../"))
-
+print("yo")
 if __name__ == '__main__':
     from .__main__ import run
     run()
