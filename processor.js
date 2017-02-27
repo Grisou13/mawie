b6db5983507d637a043eeb226679823ae21dd2b8
Thomas Ricci <grisou13@hotmail.fr>
2017-02-27 15:55:05 +0100

    Create processor.js

---
+++ processor.js
@@ -0,0 +1,9 @@
+// headings trigger a new slide
+// headings with a caret (e.g., '##^ foo`) trigger a new vertical slide
+module.exports = (markdown, options) => {
+  return markdown.split('\n').map((line, index) => {
+    if(!/^#/.test(line) || index === 0) return line;
+    const is_vertical = /#\^/.test(line);
+    return (is_vertical ? '\n----\n\n' : '\n---\n\n') + line.replace('#^', '#');
+  }).join('\n');
+};
